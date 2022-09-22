3661f7cbcad2bea6fcd444d1a4872d442edcc394
ClintonLaptop <clintieleroux@gmail.com>
2022-09-22 21:54:01 +0200

    Creating Projects

---
+++ Kanban.App/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Kanban.App
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Kanban.App/MauiProgram.cs
@@ -0,0 +1,28 @@
+﻿using Kanban.App.Data;
+using Microsoft.AspNetCore.Components.WebView.Maui;
+
+namespace Kanban.App
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+
+            builder.Services.AddMauiBlazorWebView();
+#if DEBUG
+		builder.Services.AddBlazorWebViewDeveloperTools();
+#endif
+
+            builder.Services.AddSingleton<WeatherForecastService>();
+
+            return builder.Build();
+        }
+    }
+}
